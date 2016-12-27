b2877bbfdd6e76e2a8302d483c3726580dd3ae0d
Hwa-Young Choi <chyccs@naver.com>
2016-12-27 11:19:15 +0900

    api document import

---
+++ docs/class_neosmartpen_1_1_net_1_1_bluetooth_1_1_bluetooth_pen_client.js
@@ -0,0 +1,14 @@
+var class_neosmartpen_1_1_net_1_1_bluetooth_1_1_bluetooth_pen_client =
+[
+    [ "Bind", "class_neosmartpen_1_1_net_1_1_bluetooth_1_1_bluetooth_pen_client.html#a6f2dc553557717b1850a0843d1482b06", null ],
+    [ "Connect", "class_neosmartpen_1_1_net_1_1_bluetooth_1_1_bluetooth_pen_client.html#a545651ac7a6ccb280a7c7635b63b83a1", null ],
+    [ "Connect", "class_neosmartpen_1_1_net_1_1_bluetooth_1_1_bluetooth_pen_client.html#a93611f3bc5ff076175b5423da2cd20fc", null ],
+    [ "Disconnect", "class_neosmartpen_1_1_net_1_1_bluetooth_1_1_bluetooth_pen_client.html#ac12a5ab861ddb69413758317d6bd2571", null ],
+    [ "FindDevices", "class_neosmartpen_1_1_net_1_1_bluetooth_1_1_bluetooth_pen_client.html#a69990905588cb801474fc03e1efaead8", null ],
+    [ "FindPairedDevices", "class_neosmartpen_1_1_net_1_1_bluetooth_1_1_bluetooth_pen_client.html#a8b002361d659d8b0dc09c263005e177b", null ],
+    [ "Pairing", "class_neosmartpen_1_1_net_1_1_bluetooth_1_1_bluetooth_pen_client.html#a91dad32c6544924cb796d3be2c98d1e0", null ],
+    [ "Read", "class_neosmartpen_1_1_net_1_1_bluetooth_1_1_bluetooth_pen_client.html#ad50b7cca334f5ce8bb0e7d5d49c3f026", null ],
+    [ "Unbind", "class_neosmartpen_1_1_net_1_1_bluetooth_1_1_bluetooth_pen_client.html#a171d27254a4ba66bdb37078bb9126863", null ],
+    [ "UnPairing", "class_neosmartpen_1_1_net_1_1_bluetooth_1_1_bluetooth_pen_client.html#aaf598fbdb7bea776711053231dc5f2a6", null ],
+    [ "Write", "class_neosmartpen_1_1_net_1_1_bluetooth_1_1_bluetooth_pen_client.html#a311445e69d0a4ebf765640a272cba315", null ]
+];
---
+++ docs/hierarchy.js
@@ -0,0 +1,33 @@
+var hierarchy =
+[
+    [ "Neosmartpen.Net.BatteryAlarmReceivedEventArgs", "class_neosmartpen_1_1_net_1_1_battery_alarm_received_event_args.html", null ],
+    [ "Neosmartpen.Net.Dot.Builder", "class_neosmartpen_1_1_net_1_1_dot_1_1_builder.html", null ],
+    [ "Neosmartpen.Net.ConnectedEventArgs", "class_neosmartpen_1_1_net_1_1_connected_event_args.html", null ],
+    [ "Neosmartpen.Net.Dot", "class_neosmartpen_1_1_net_1_1_dot.html", null ],
+    [ "Neosmartpen.Net.DotReceivedEventArgs", "class_neosmartpen_1_1_net_1_1_dot_received_event_args.html", null ],
+    [ "Exception", null, [
+      [ "Neosmartpen.Net.PenRequestException", "class_neosmartpen_1_1_net_1_1_pen_request_exception.html", [
+        [ "Neosmartpen.Net.RequestIsUnreached", "class_neosmartpen_1_1_net_1_1_request_is_unreached.html", null ],
+        [ "Neosmartpen.Net.UnavailableRequest", "class_neosmartpen_1_1_net_1_1_unavailable_request.html", null ]
+      ] ]
+    ] ],
+    [ "Neosmartpen.Net.IPenClient", "interface_neosmartpen_1_1_net_1_1_i_pen_client.html", [
+      [ "Neosmartpen.Net.PenClient", "class_neosmartpen_1_1_net_1_1_pen_client.html", [
+        [ "Neosmartpen.Net.Bluetooth.BluetoothPenClient", "class_neosmartpen_1_1_net_1_1_bluetooth_1_1_bluetooth_pen_client.html", null ]
+      ] ]
+    ] ],
+    [ "Neosmartpen.Net.IPenController", "interface_neosmartpen_1_1_net_1_1_i_pen_controller.html", [
+      [ "Neosmartpen.Net.PenController", "class_neosmartpen_1_1_net_1_1_pen_controller.html", null ]
+    ] ],
+    [ "List", null, [
+      [ "Neosmartpen.Net.Stroke", "class_neosmartpen_1_1_net_1_1_stroke.html", null ]
+    ] ],
+    [ "Neosmartpen.Net.OfflineDataListReceivedEventArgs", "class_neosmartpen_1_1_net_1_1_offline_data_list_received_event_args.html", null ],
+    [ "Neosmartpen.Net.OfflineStrokeReceivedEventArgs", "class_neosmartpen_1_1_net_1_1_offline_stroke_received_event_args.html", null ],
+    [ "Neosmartpen.Net.PasswordRequestedEventArgs", "class_neosmartpen_1_1_net_1_1_password_requested_event_args.html", null ],
+    [ "Neosmartpen.Net.Bluetooth.PenInformation", "class_neosmartpen_1_1_net_1_1_bluetooth_1_1_pen_information.html", null ],
+    [ "Neosmartpen.Net.PenStatusReceivedEventArgs", "class_neosmartpen_1_1_net_1_1_pen_status_received_event_args.html", null ],
+    [ "Neosmartpen.Net.Bluetooth.PenUpdateInformation", "class_neosmartpen_1_1_net_1_1_bluetooth_1_1_pen_update_information.html", null ],
+    [ "Neosmartpen.Net.ProgressChangeEventArgs", "class_neosmartpen_1_1_net_1_1_progress_change_event_args.html", null ],
+    [ "Neosmartpen.Net.SimpleResultEventArgs", "class_neosmartpen_1_1_net_1_1_simple_result_event_args.html", null ]
+];
