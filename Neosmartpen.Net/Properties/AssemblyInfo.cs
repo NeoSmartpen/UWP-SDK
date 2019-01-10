759ae68dd29e91b13efe37452825d374ba3003d8
Hwa-Young Choi <chyccs@naver.com>
2019-01-10 10:55:50 +0900

    v2.1.147

--- Neosmartpen.Net/Properties/AssemblyInfo.cs
+++ Neosmartpen.Net/Properties/AssemblyInfo.cs
@@ -23,6 +23,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.146")]
-[assembly: AssemblyFileVersion("2.1.146")]
+[assembly: AssemblyVersion("2.1.147")]
+[assembly: AssemblyFileVersion("2.1.147")]
 [assembly: ComVisible(false)]
